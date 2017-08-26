7263e8b5cf7dcdda6b425a446c2d0b1e388a8859
Chris <sjhshy@gmail.com>
2017-08-26 14:14:01 +0900

    디바<3 클리커 게임 - 초마다 골드 추가

---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataController : MonoBehaviour {
+
+
+	// Singleton class start
+	static GameObject _container;
+	static GameObject Container {
+		get {
+			return _container;
+		}
+	}
+
+	static DataController _instance;
+	public static DataController Instance {
+		get {
+			if( ! _instance ) {
+				_container = new GameObject();
+				_container.name = "DataController";
+				_instance = _container.AddComponent( typeof(DataController) ) as DataController;
+				DontDestroyOnLoad (_container);
+			}
+
+			return _instance;
+		}
+	}
+	// Singleton class end
+
+	public int Gold = 0;
+
+	public int GoldPerSec = 1;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Text TextGold;
+
+	// Use this for initialization
+	void Start () {
+		TextGold.text = DataController.Instance.Gold.ToString();
+		StartCoroutine (StartCollectGold ());
+	}
+
+	IEnumerator StartCollectGold() {
+
+		while (true) {
+		
+			yield return new WaitForSecondsRealtime (1f);
+			DataController.Instance.Gold += DataController.Instance.GoldPerSec;
+			TextGold.text = DataController.Instance.Gold.ToString();
+
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
