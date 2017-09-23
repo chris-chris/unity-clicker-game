fddaf372a8bb2c534c0c3ae1e0288a168ade9b05
Chris <sjhshy@gmail.com>
2017-09-23 16:05:29 +0900

    facebook 연동 버전

---
+++ Assets/Scripts/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+
+public class LoginController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoginFacebook(){
+		
+		FB.Init ( delegate() {
+			
+			FB.LogInWithReadPermissions(
+
+				new List<string>(){"public_profile", "email", "user_friends"},
+
+				delegate(ILoginResult result) {
+				
+					if(result.Error == null){
+
+						LoadFacebookMe();
+
+						//Application.LoadLevel("Game");
+					}
+
+					Debug.Log(result.RawResult);
+
+				});
+			
+		});
+	}
+
+	public void LoadFacebookMe(){
+		FB.API ("/me", HttpMethod.GET, delegate(IGraphResult result) {
+			Debug.Log(result.RawResult);
+			Debug.Log(result.ResultDictionary["name"]);
+			Debug.Log(result.ResultDictionary["id"]);
+			Debug.Log("https://graph.facebook.com/" + result.ResultDictionary["id"] + "/picture");
+			LoadFacebookFriends();
+		});
+	}
+
+	public void LoadFacebookFriends(){
+	
+		FB.API ("/me/friends", HttpMethod.GET, delegate(IGraphResult result) {
+			Debug.Log(result.RawResult);
+		});
+
+	}
+
+}
