179eddac189ad4323b307e6b21de32c218b64033
Chris Song <chris.song@kakaocorp.com>
2017-11-25 18:10:19 +0900

    1주차 완성

---
+++ Assets/Scripts/GoldCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldCounter : MonoBehaviour {
+
+	public int Gold = 0;
+	public Text TextGold;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (StartGoldCount ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator StartGoldCount(){
+		while (true) {
+			yield return new WaitForSecondsRealtime (1f);
+			Gold++;
+			TextGold.text = Gold.ToString ();
+		}
+	}
+}
--- Assets/Scripts/LoginController.cs
+++ Assets/Scripts/LoginController.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Facebook.Unity;
+using UnityEngine.SceneManagement;
 
 public class LoginController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 	
 	// Update is called once per frame
@@ -15,46 +15,7 @@ public class LoginController : MonoBehaviour {
 		
 	}
 
-	public void LoginFacebook(){
-		
-//		FB.Init ( delegate() {
-//			
-//			FB.LogInWithReadPermissions(
-//
-//				new List<string>(){"public_profile", "email", "user_friends"},
-//
-//				delegate(ILoginResult result) {
-//				
-//					if(result.Error == null){
-//
-//						LoadFacebookMe();
-//
-//						//Application.LoadLevel("Game");
-//					}
-//
-//					Debug.Log(result.RawResult);
-//
-//				});
-//			
-//		});
+	public void MoveGame(){
+		SceneManager.LoadScene ("Game");
 	}
-
-	public void LoadFacebookMe(){
-//		FB.API ("/me", HttpMethod.GET, delegate(IGraphResult result) {
-//			Debug.Log(result.RawResult);
-//			Debug.Log(result.ResultDictionary["name"]);
-//			Debug.Log(result.ResultDictionary["id"]);
-//			Debug.Log("https://graph.facebook.com/" + result.ResultDictionary["id"] + "/picture");
-//			LoadFacebookFriends();
-//		});
-	}
-
-	public void LoadFacebookFriends(){
-	
-//		FB.API ("/me/friends", HttpMethod.GET, delegate(IGraphResult result) {
-//			Debug.Log(result.RawResult);
-//		});
-
-	}
-
 }
