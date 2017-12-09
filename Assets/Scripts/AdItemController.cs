a14b8c45ed099662df6e101f15676aaa6682d44d
Chris Song <chris.song@kakaocorp.com>
2017-12-09 16:30:55 +0900

    유니티 동영상 광고 구현

---
+++ Assets/Scripts/AdItemController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdItemController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ShowRewardedVideo ()
+	{
+		ShowOptions options = new ShowOptions();
+		options.resultCallback = HandleShowResult;
+
+		Advertisement.Show("rewardedVideo", options);
+	}
+
+	void HandleShowResult (ShowResult result)
+	{
+		if(result == ShowResult.Finished) {
+			Debug.Log("Video completed - Offer a reward to the player");
+
+			DataController.Instance.gameData.Gold += 5000;
+			DataController.Instance.SaveGameData ();
+			NotificationCenter.Instance.Notify ("GoldUpdate");
+
+		}else if(result == ShowResult.Skipped) {
+			Debug.LogWarning("Video was skipped - Do NOT reward the player");
+
+		}else if(result == ShowResult.Failed) {
+			Debug.LogError("Video failed to show");
+		}
+	}
+}
