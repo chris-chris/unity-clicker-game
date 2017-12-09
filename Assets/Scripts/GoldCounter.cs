2355530c91fef8454f2f8a3dcc85e1a940a01f77
Chris Song <chris.song@kakaocorp.com>
2017-12-09 14:34:38 +0900

    3주차 진행중

--- Assets/Scripts/GoldCounter.cs
+++ Assets/Scripts/GoldCounter.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class GoldCounter : MonoBehaviour {
 
 	public Text TextGold;
+	public Camera MainCamera;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +17,24 @@ public class GoldCounter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (Input.GetMouseButtonDown (0)) {
+
+			DataController.Instance.gameData.Gold += DataController.Instance.gameData.GoldPerSec;
+			TextGold.text = DataController.Instance.gameData.Gold.ToString ();
+
+			Ray ray = MainCamera.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit, 100f)) {
+
+				GameObject StarSFX = Resources.Load ("Prefabs/Stars") as GameObject;
+				Instantiate (StarSFX, hit.point, new Quaternion());
+
+			}
+
+		}
+
 	}
 
 	IEnumerator StartGoldCount(){
---
+++ Assets/Scripts/KillStar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillStar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 0.7f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
